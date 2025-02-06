8e0d1ee2cb2229d8de7126727d2e34799a66ea45
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-02-06 20:11:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let hour = document.querySelector(".hour-hand");
+let minute = document.querySelector(".min-hand");
+let second = document.querySelector(".second-hand");
+ 
+let secondDeg = 0; 
+setInterval(()=> {
+	secondDeg+=6;
+	second.style.rotate = `${secondDeg}deg`;
+}, 1000)
+
+let minuteDeg = 0;
+setInterval(()=>{
+	minuteDeg += 6;
+	minute.style.rotate = `${minuteDeg}deg`;
+}, 1000*60)
+
+let hourDeg = 0;
+setInterval(() => {
+	hourDeg += 30;
+	hour.style.rotate = `${hourDeg}deg`;
+}, 1000*60*60)
